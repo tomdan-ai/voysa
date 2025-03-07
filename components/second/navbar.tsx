3d80be5b4b8af68c5a39e4f5f41109006a91aa00
Mikel Edwin <michealanthonyxxx@gmail.com>
2025-03-08 00:37:08 +0100

    Fixed CSS issue on navbar, went back to webp

--- components/second/navbar.tsx
+++ components/second/navbar.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
-import blockchainImage from "@/images/blockchain-bg.jpg";
+import blockchainImage from "@/images/blockchain-bg.webp";
 
 const Navbar = () => {
   return (
     <nav
       className="fixed top-0 left-0 w-full z-10 backdrop-blur-2xl shadow-sm bg-center bg-cover bg-no-repeat"
       style={{
-        backgroundImage: `linear-gradient(to #222, black, transparent 100%), url('${blockchainImage.src}')`,
+        backgroundImage: `linear-gradient(to bottom, #0a0a0a, transparent 100%), url('${blockchainImage.src}')`,
       }}
     >
       <div className="container mx-auto p-4 flex items-center text-white justify-between">
